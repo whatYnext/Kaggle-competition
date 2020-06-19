59f9767917ce1493eefade846b41bc4169306437
whatYnext <63004699+whatYnext@users.noreply.github.com>
2020-06-19 20:20:41 +0200

    House_price_prediction

---
+++ House_price_prediction.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 15 15:11:47 2020
+
+@author: mayao
+"""
+
+import d2lzh as d2l
+from mxnet import autograd, gluon, init, nd
+from mxnet.gluon import data as gdata, loss as gloss, nn
+import numpy as np
+import pandas as pd
+
+train_data = pd.read_csv('C:/Users/mayao/Desktop/Python learning/Kaggle/House price/data/train.csv')
+test_data = pd.read_csv('C:/Users/mayao/Desktop/Python learning/Kaggle/House price/data/test.csv')
+
+#Deliminate first column (index column)
+all_features = pd.concat((train_data.iloc[:, 1:-1], test_data.iloc[:, 1:]))
+
+# Preprocess data
+numeric_features = all_features.dtypes[all_features.dtypes != 'object'].index
+all_features[numeric_features] = all_features[numeric_features].apply(
+    lambda x: (x - x.mean()) / (x.std()))
+# Use 0 to replace NA terms
+all_features[numeric_features] = all_features[numeric_features].fillna(0)
+
+# Extend data at dummy position
+all_features = pd.get_dummies(all_features, dummy_na=True)
+
+# Turn to numpy type
+n_train = train_data.shape[0]
+train_features = nd.array(all_features[:n_train].values)
+test_features = nd.array(all_features[n_train:].values)
+train_labels = nd.array(train_data.SalePrice.values).reshape((-1, 1))
+
+### The detailed implementation ###
+
+# loss function and one dense layer
+loss = gloss.L2Loss()
+
+def get_net():
+    net = nn.Sequential()
+    net.add(nn.Dense(1))
+    net.initialize()
+    return net
+
+#log average error
+def log_rmse(net, features, labels):
+    # Make number which is less than 1 as 1
+    clipped_preds = nd.clip(net(features), 1, float('inf'))
+    rmse = nd.sqrt(2 * loss(clipped_preds.log(), labels.log()).mean())
+    return rmse.asscalar()
+
+# Train function
+def train(net, train_features, train_labels, test_features, test_labels,
+          num_epochs, learning_rate, weight_decay, batch_size):
+    train_ls, test_ls = [], []
+    train_iter = gdata.DataLoader(gdata.ArrayDataset(
+        train_features, train_labels), batch_size, shuffle=True)
+    # Use Adam algorithm
+    trainer = gluon.Trainer(net.collect_params(), 'adam', {
+        'learning_rate': learning_rate, 'wd': weight_decay})
+    for epoch in range(num_epochs):
+        for X, y in train_iter:
+            with autograd.record():
+                l = loss(net(X), y)
+            l.backward()
+            trainer.step(batch_size)
+        train_ls.append(log_rmse(net, train_features, train_labels))
+        if test_labels is not None:
+            test_ls.append(log_rmse(net, test_features, test_labels))
+    return train_ls, test_ls
+
+# Get k fold data
+def get_k_fold_data(k, i, X, y):
+    assert k > 1
+    fold_size = X.shape[0] // k
+    X_train, y_train = None, None
+    for j in range(k):
+        idx = slice(j * fold_size, (j + 1) * fold_size)
+        X_part, y_part = X[idx, :], y[idx]
+        if j == i:
+            X_valid, y_valid = X_part, y_part
+        elif X_train is None:
+            X_train, y_train = X_part, y_part
+        else:
+            X_train = nd.concat(X_train, X_part, dim=0)
+            y_train = nd.concat(y_train, y_part, dim=0)
+    return X_train, y_train, X_valid, y_valid
+
+#Train on k fold data
+def k_fold(k, X_train, y_train, num_epochs,
+           learning_rate, weight_decay, batch_size):
+    train_l_sum, valid_l_sum = 0, 0
+    for i in range(k):
+        data = get_k_fold_data(k, i, X_train, y_train)
+        net = get_net()
+        train_ls, valid_ls = train(net, *data, num_epochs, learning_rate,
+                                   weight_decay, batch_size)
+        train_l_sum += train_ls[-1]
+        valid_l_sum += valid_ls[-1]
+        if i == 0:
+            d2l.semilogy(range(1, num_epochs + 1), train_ls, 'epochs', 'rmse',
+                         range(1, num_epochs + 1), valid_ls,
+                         ['train', 'valid'])
+        print('fold %d, train rmse %f, valid rmse %f'
+              % (i, train_ls[-1], valid_ls[-1]))
+    return train_l_sum / k, valid_l_sum / k
+
+k, num_epochs, lr, weight_decay, batch_size = 5, 100, 3, 0, 32
+# train_l, valid_l = k_fold(k, train_features, train_labels, num_epochs, lr,
+#                           weight_decay, batch_size)
+# print('%d-fold validation: avg train rmse %f, avg valid rmse %f' % (k, train_l, valid_l))
+
+def train_and_pred(train_features, test_features, train_labels, test_data,
+                   num_epochs, lr, weight_decay, batch_size):
+    net = get_net()
+    train_ls, _ = train(net, train_features, train_labels, None, None,
+                        num_epochs, lr, weight_decay, batch_size)
+    d2l.semilogy(range(1, num_epochs + 1), train_ls, 'epochs', 'rmse')
+    print('train rmse %f' % train_ls[-1])
+    preds = net(test_features).asnumpy()
+    test_data['SalePrice'] = pd.Series(preds.reshape(1, -1)[0])
+    submission = pd.concat([test_data['Id'], test_data['SalePrice']], axis=1)
+    submission.to_csv('submission.csv', index=False)
+
+# Train and predict    
+train_and_pred(train_features, test_features, train_labels, test_data,
+               num_epochs, lr, weight_decay, batch_size)    
